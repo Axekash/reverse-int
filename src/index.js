7da2b818d013b514a5ae41515d9ef94c5e5e31c5
Aliaksandr 'Axekash' Kuchutkov <kashim007@mail.ru>
2022-02-21 01:18:38 +0300

    Reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let i = 0;
+    let str = String(Math.abs(n));
+    let numb = '';
+
+
+  while (i < str.length) {
+    numb =  str[i] + numb;
+    i = i + 1;
+  };
+
+  return Number(numb);
 }
